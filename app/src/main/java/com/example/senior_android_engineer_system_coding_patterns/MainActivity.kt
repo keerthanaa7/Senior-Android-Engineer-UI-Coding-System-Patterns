0f759c9e44ed39fa7c2509ccf0f51a501020a01e
keerthanaa7 <keerthanaa1959@gmail.com>
2026-05-17 20:04:04 -0700

    senior android ui coding patterns

---
+++ app/src/main/java/com/example/senior_android_engineer_system_coding_patterns/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.senior_android_engineer_system_coding_patterns
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.senior_android_engineer_system_coding_patterns.ui.theme.SeniorAndroidEngineerSystemCodingPatternsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SeniorAndroidEngineerSystemCodingPatternsTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SeniorAndroidEngineerSystemCodingPatternsTheme {
+        Greeting("Android")
+    }
+}
